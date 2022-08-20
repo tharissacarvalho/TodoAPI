b6283465870234d16ded809429b8009917c4b28f
Tharissa <tharissaetec@gmail.com>
2022-08-20 16:57:28 -0300

    feat: Adicionando ResponseDto

---
+++ src/Kobold.TodoApp.Api/Dtos/ResponseDto.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+
+namespace Kobold.TodoApp.Api.Dtos
+{
+    public class ResponseDto
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public object Response { get; set; }
+
+        public static ResponseDto ReturnStatusCode(HttpStatusCode statusCode, object response)
+        {
+            return new ResponseDto()
+            {
+                StatusCode = statusCode,
+                Response = response
+            };
+        }
+        public static ResponseDto Ok(object response)
+        {
+            return ReturnStatusCode(HttpStatusCode.OK, response);
+        }
+
+        public static ResponseDto InternalServerError(object response)
+        {
+            return ReturnStatusCode(HttpStatusCode.InternalServerError, response);
+        }
+
+        public static ResponseDto NotFoundException(object response)
+        {
+            return ReturnStatusCode(HttpStatusCode.NotFound, response);
+        }
+    }
+}
